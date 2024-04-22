4060fbc8d104a396e81a223fc56568872e0c313b
MrDumbGuy <94762039+MrDumbGuy@users.noreply.github.com>
2024-04-22 21:25:45 +0300

    Updated for new version
    More color support, somewhat stable fps in the works.

--- videotoraw.py
+++ videotoraw.py
@@ -1,10 +1,12 @@
 import cv2
+import matplotlib.colors
 import numpy as np
+import matplotlib
 
-f = open("data.txt", 'a')
+f = open("datacol.txt", 'a')
 
-def get_color_name(pixel):
-    colors = {
+colors = { #light = lowercase, dark = uppercase. Encoded as BGR here because I have no clue how HSL works. 
+           #Tip: Don't bother trying to add gray. Everything gets approximated to gray.
         'r': (0, 0, 255),
         'R': (0, 0, 139),
         'y': (0, 255, 255),
@@ -12,27 +14,43 @@ def get_color_name(pixel):
         'g': (0, 255, 0),
         'G': (0, 139, 0),
         'b': (255, 0, 0),
-        'B': (139, 0, 0),
+        'B': (139, 32, 0),
         'c': (0, 255, 255),
-        'C': (0, 139, 139),
+        'C': (0, 139, 255),
         'm': (255, 0, 255),
         'M': (139, 0, 139),
         '0': (0, 0, 0),
         '1': (255, 255, 255),
-    }
+        '2': (128, 128, 128),
+        '3': (169, 169, 169),
+        '4': (153, 136, 119),
+        '5': (79, 79, 47),
+        'o': (0, 165, 255),
+        'O': (0, 140, 255),
+        'w': (45, 82, 160),
+        'W': (19, 69, 139),
+        }
+    
+for c in colors:
+    colors[c] = matplotlib.colors.rgb_to_hsv(colors[c]) #HSV is more accurate to human perception than RGB.
+    print(colors[c])
 
+def get_color_name(pixel):
     min_dist = float('inf')
     color_name = None
-
+    pixel_hsv = matplotlib.colors.rgb_to_hsv(pixel)
+    
     for color, value in colors.items():
-        dist = np.linalg.norm(np.array(pixel) - np.array(value))
+        dist = np.linalg.norm(np.array(pixel_hsv) - np.array(value))
+        
         if dist < min_dist:
             min_dist = dist
             color_name = color
 
     return color_name
 
-cap = cv2.VideoCapture('video.mp4')
+
+cap = cv2.VideoCapture('cpmcfs.mp4') #Replace this with your own video!
 
 while cap.isOpened():
     ret, frame = cap.read()
