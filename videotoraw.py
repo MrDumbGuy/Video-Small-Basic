ffff128472d08047f2aac168a02ec0d53289f31f
MrDumbGuy <94762039+MrDumbGuy@users.noreply.github.com>
2024-04-30 23:18:36 +0300

    Breaking Changes Added
    Color support was dropped in favour of much faster video compilation times. Videos generated on older versions will no longer be compatible.

--- videotoraw.py
+++ videotoraw.py
@@ -3,45 +3,30 @@ import matplotlib.colors
 import numpy as np
 import matplotlib
 
-f = open("datacol.txt", 'a')
+f = open("test.txt", 'a')
 
-colors = { #light = lowercase, dark = uppercase. Encoded as BGR here because I have no clue how HSL works. 
-           #Tip: Don't bother trying to add gray. Everything gets approximated to gray.
-        'r': (0, 0, 255),
-        'R': (0, 0, 139),
-        'y': (0, 255, 255),
-        'Y': (0, 139, 139),
-        'g': (0, 255, 0),
-        'G': (0, 139, 0),
-        'b': (255, 0, 0),
-        'B': (139, 32, 0),
-        'c': (0, 255, 255),
-        'C': (0, 139, 255),
-        'm': (255, 0, 255),
-        'M': (139, 0, 139),
-        '0': (0, 0, 0),
-        '1': (255, 255, 255),
-        '2': (128, 128, 128),
-        '3': (169, 169, 169),
-        '4': (153, 136, 119),
-        '5': (79, 79, 47),
-        'o': (0, 165, 255),
-        'O': (0, 140, 255),
-        'w': (45, 82, 160),
-        'W': (19, 69, 139),
-        }
+colors = {
+    '0': (0, 0, 0),
+    '1': (26, 26, 26),
+    '2': (51, 51, 51),
+    '3': (77, 77, 77),
+    '4': (102, 102, 102),
+    '5': (128, 128, 128),
+    '6': (153, 153, 153),
+    '7': (255, 255, 255),
+    }
     
 for c in colors:
-    colors[c] = matplotlib.colors.rgb_to_hsv(colors[c]) #HSV is more accurate to human perception than RGB.
+    colors[c] = sum(colors[c])/len(colors[c])
     print(colors[c])
 
 def get_color_name(pixel):
+    pixel = sum(pixel)/len(pixel)
     min_dist = float('inf')
     color_name = None
-    pixel_hsv = matplotlib.colors.rgb_to_hsv(pixel)
     
     for color, value in colors.items():
-        dist = np.linalg.norm(np.array(pixel_hsv) - np.array(value))
+        dist = abs(value - pixel)
         
         if dist < min_dist:
             min_dist = dist
@@ -50,7 +35,7 @@ def get_color_name(pixel):
     return color_name
 
 
-cap = cv2.VideoCapture('cpmcfs.mp4') #Replace this with your own video!
+cap = cv2.VideoCapture('input.mp4') #Replace this with your own video!
 
 while cap.isOpened():
     ret, frame = cap.read()
